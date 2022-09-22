981aa3934507206c6fe27afab3dd8c07232b4399
Afthab69 <afthabsajad3@gmail.com>
2022-09-22 12:23:52 +0530

    new tasks

---
+++ Training/src/com/orthofx/MatrixMultipliedWithAConstant.java
@@ -0,0 +1,45 @@
+package com.orthofx;
+
+import java.util.Scanner;
+
+public class MatrixMultipliedWithAConstant {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int matrix[][] = new int[10][10];
+		int result[][] = new int[10][10];
+		int r,c,k;
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter dimension of matrix: ");
+		r = s.nextInt();
+		c = s.nextInt();
+		System.out.println("Enter elements of matrix : ");
+		for(int i=0;i<r;++i) {
+			for(int j=0;j<c;++j) {
+				matrix[i][j] = s.nextInt();
+			}
+		}
+		System.out.println("Matrix:");
+		for(int i=0;i<r;++i) {
+			for(int j=0;j<c;++j) {
+				System.out.print(matrix[i][j]+" ");
+			}
+			System.out.println();
+		}
+		System.out.println("Enter number to be multiplied:");
+		k = s.nextInt();
+		for(int i=0;i<r;++i) {
+			for(int j=0;j<c;++j) {
+				result[i][j] = k*matrix[i][j];
+			}
+		}
+		System.out.println("Resultant Matrix:");
+		for(int i=0;i<r;++i) {
+			for(int j=0;j<c;++j) {
+				System.out.print(result[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
--- Training/src/com/orthofx/PyramidUsingWhileLoop.java
+++ Training/src/com/orthofx/PyramidUsingWhileLoop.java
@@ -6,22 +6,22 @@ public class PyramidUsingWhileLoop {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int  n,i=0,j=0,k=0;
+		int n, i = 0, j = 0, k = 0;
 		Scanner s = new Scanner(System.in);
 		System.out.println("Enter value of n: ");
 		n = s.nextInt();
-		while(i<n) {
-			j=0;
-			k=0;
-			while(j<n-i) {
+		while (i < n) {
+			j = 0;
+			k = 0;
+			while (j < n - i) {
 				System.out.print(" ");
 				j++;
 			}
-			while(k<=i) {
+			while (k <= i) {
 				System.out.print("* ");
 				k++;
 			}
-			System.out.println(); 
+			System.out.println();
 			i++;
 		}
 	}
